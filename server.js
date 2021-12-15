8cfe997c41c22462a4a6232c6ae1ee6675bb48d1
canyetgin <cannyetgin@gmail.com>
2021-12-15 20:18:29 +0300

    User register, jwt implementing

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/profile", require("./routes/profile"));
-app.use("/api/posts", require("./routes/posts"));
+app.use(express.json({
+    extended: false
+}));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
