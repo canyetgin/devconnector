9825b37d23359b062698827ce0056be0a689525f
canyetgin <cannyetgin@gmail.com>
2021-12-17 10:30:57 +0300

    Get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,9 @@ const Profile = require("../../models/Profile");
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+const request = require("request");
+const config = require("config");
+
 router.get(
     '/me',
     auth,
@@ -289,6 +292,35 @@ router.delete(
         }
     }
 );
+
+router.get("/github/:username",
+    (req, res) => {
+     try{
+
+         const options = {
+             url: `https://api.github.com/users/
+             ${req.params.username}/repos?per_page=5&sort=created:asc&client_id=
+             ${config.get("githubClientId")}&client_secret=
+             ${config.get("githubSecret")}`,
+             method: "GET",
+             headers: {
+                 "user-agent": "node.js"
+             }
+         }
+
+         request(options, (error, response, body) => {
+            if(error) console.error(error);
+            if(response.statusCode !== 200){
+                return res.status(404).json({ msg: "No Github profile found" });
+            }
+            res.json(JSON.parse(body));
+         });
+
+     }catch (e) {
+         console.error(e.message);
+         res.status(500).send("Server Error");
+     }
+    });
 
 
 module.exports = router;
