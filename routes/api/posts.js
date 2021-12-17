3184a7c0c26c4ecdcfb767b3d3f27433f588f7e7
canyetgin <cannyetgin@gmail.com>
2021-12-17 20:30:27 +0300

    Post like, dislike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -96,4 +96,53 @@ router.delete(
         }
     });
 
+router.put(
+    "/like/:id",
+    auth,
+    async (req, res) => {
+        try{
+            const post = await Post.findById(req.params.id);
+            if(!post){
+                return res.status(404).json({ msg: "Post not found" });
+            }
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                return res.status(400).json({ msg: "Post already liked" });
+            }
+            post.likes.unshift({ user: req.user.id });
+            await post.save();
+            return res.json(post.likes);
+        }catch (e) {
+            console.error(e.message);
+            if(e.kind === "ObjectId") return res.status(404).json({ msg: "Post not found" });
+            res.status(500).send("Server Error");
+        }
+
+    }
+);
+
+router.put(
+    "/unlike/:id",
+    auth,
+    async (req, res) => {
+        try{
+            const post = await Post.findById(req.params.id);
+            if(!post){
+                return res.status(404).json({ msg: "Post not found" });
+            }
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                return res.status(400).json({ msg: "Post has not yet been liked" });
+            }
+            const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+            post.likes.splice(removeIndex, 1);
+            await post.save();
+            return res.json(post.likes);
+        }catch (e) {
+            console.error(e.message);
+            if(e.kind === "ObjectId") return res.status(404).json({ msg: "Post not found" });
+            res.status(500).send("Server Error");
+        }
+
+    }
+);
+
 module.exports = router;
