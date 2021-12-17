cd6101cbe0f84fa3469b01da9f87cc35a85a5ce2
canyetgin <cannyetgin@gmail.com>
2021-12-17 20:36:21 +0300

    Add, Remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
-const Profile = require("../../models/Profile");
 const Post = require("../../models/Post");
 router.post(
     '/',
@@ -144,5 +143,72 @@ router.put(
 
     }
 );
+
+router.post(
+    '/comment/:id',
+    auth,
+    check('text', 'Text is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+            if(!post){
+                return res.status(404).json({ msg: "Post not found" });
+            }
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            };
+
+            post.comments.unshift(newComment);
+
+            await post.save();
+
+            res.json(post.comments);
+        } catch (e) {
+            console.error(e.message);
+            if(e.kind === "ObjectId") return res.status(404).json({ msg: "Post not found" });
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({ msg: "Post not found" });
+        }
+        const comment = post.comments.find(
+            (comment) => comment.id === req.params.comment_id
+        );
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+        // Check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        post.comments = post.comments.filter(
+            ({ id }) => id !== req.params.comment_id
+        );
+
+        await post.save();
+
+        return res.json(post.comments);
+    } catch (e) {
+        console.error(e.message);
+        if(e.kind === "ObjectId") return res.status(404).json({ msg: "Post not found" });
+        return res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
